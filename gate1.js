176c220b788162cdd5a2ef1c837ba5da87d55f29
Thota Ramakonda Kishore <149231644+thotaramakishore@users.noreply.github.com>
2023-10-28 18:33:20 +0530

    Add files via upload

---
+++ gate1.js
@@ -0,0 +1,6 @@
+function gate_open(){
+    document.getElementById('image').src="opendoordesign.png";
+}
+function gate_close(){
+    document.getElementById('image').src = "closedoordesign.png";
+}
